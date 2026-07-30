e654c1e4c2de288db3541363f33bc07e5ed9f37a
neyadrian <neyadrian2018@gmail.com>
2026-07-30 15:46:29 -0300

    exemplo didático pt4

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,9 @@ public class Main {
         double grossSalary = sc.nextDouble();
 
         Employee employee = new Employee(name, grossSalary);
-        SalaryService salaryService = new SalaryService();
+        TaxService taxService = new TaxService;
+        PensionService pensionService = new PensionService();
+        SalaryService salaryService = new SalaryService(taxService, pensionService);
 
         double netSalary = salaryService.netSalary(employee);
         System.out.printf("Salário liquido = %.2f\n", netSalary);
