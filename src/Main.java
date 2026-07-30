849ef23dc1a534ffb17d7fe80bbbbfe7db58e66e
neyadrian <neyadrian2018@gmail.com>
2026-07-30 15:23:37 -0300

    exemplo didático pt2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import services.PensionService;
+import services.TaxService;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +10,13 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        TaxService taxService = new TaxService();
+        PensionService pensionService = new PensionService();
+
+        System.out.println(taxService.tax(4000.00));
+        System.out.println(pensionService.discount(4000.00));
+
+        /*
         System.out.print("Nome: ");
         String name = sc.nextLine();
         System.out.print("Salário bruto: ");
@@ -14,6 +24,8 @@ public class Main {
 
         double netSalary = grossSalary * 0.7;
         System.out.printf("Salário liquido = %.2f\n", netSalary);
+        */
+
 
         sc.close();
 
